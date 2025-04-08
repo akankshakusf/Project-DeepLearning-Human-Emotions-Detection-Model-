346519b303ab908a6d486e96a8a434c5ac2f8785
Akanksha Kushwaha <151656066+akankshakusf@users.noreply.github.com>
2025-04-08 14:00:23 -0400

    Add files via upload

---
+++ locusts.py
@@ -0,0 +1,14 @@
+from locust import SequentialTaskSet, HttpUser, task
+
+class DetectorTask(SequentialTaskSet):
+    @task
+    def detection(self):
+        with open("test_image.jpg", "rb") as image:
+            self.client.post(
+                "/detect",
+                files={"im":image}
+            )
+
+class LoadTester(HttpUser):
+    host = "https://emotions-detection-akankshakusf-herokuapp.com",
+    tasks=[DetectorTask]
